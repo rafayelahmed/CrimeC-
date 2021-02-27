29cb3cab49c7cdea96ecc0cb8efe0f184e077499
rafa <rafayelhtc@gmail.com>
2021-02-27 23:20:56 +1100

    CD-10 c# rafayel ahmed displayed

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,15 @@ namespace CrimeDBFInal
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Rafayel Ahmed";
+        }
     }
 }
